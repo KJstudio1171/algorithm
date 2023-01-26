9a16b0f93606e3f5169be411e613ddae2fbf533f
KJstudio1171 <june1171@naver.com>
2023-01-26 16:05:27 +0900

    ADD 캐릭터의 좌표.py

---
+++ 프로그래머스/캐릭터의_좌표.py
@@ -0,0 +1,16 @@
+def solution(keyinput, board):
+    answer = [0,0]
+    for i in keyinput:
+        if i == "up":
+            if answer[1] < board[1] // 2:
+                answer[1] += 1
+        elif i == "down":
+            if answer[1] > -1 * (board[1] // 2):
+                answer[1] -= 1
+        elif i == "left":
+            if answer[0] > -1 * (board[0] // 2):
+                answer[0] -= 1
+        elif i == "right":
+            if answer[0] < board[0] // 2:
+                answer[0] += 1
+    return answer
